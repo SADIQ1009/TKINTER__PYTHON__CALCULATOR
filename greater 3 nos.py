a672d219e30a1df502e958198f6c9171872e17e7
SADIQ1009 <114791304+SADIQ1009@users.noreply.github.com>
2022-10-01 09:20:34 +0530

    Add files via upload

---
+++ greater 3 nos.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the value for a:"))
+b=int(input("Enter the value for b:"))
+c=int(input("Enter the value for c:"))
+if (a>b):
+    if (a>c):
+        print("a is greater")
+    else:
+        print("c is greater")
+else:
+    if (b>c):
+        print("b is greater")
+    else:
+        print("c is greater")
+        
